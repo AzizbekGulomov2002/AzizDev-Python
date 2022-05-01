ee94946f44f46364b27a1a3785dcfec8667a58bc
Azizbek <azizbekgulomov>
2022-05-01 16:13:28 +0500

    AzizDev

---
+++ user/models.py
@@ -0,0 +1,56 @@
+from tabnanny import verbose
+from django.db import models
+from sqlalchemy import null
+
+# Create your models here.
+class Infomatsion(models.Model):
+    frist_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    phonenumer = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',null=True,default='photos/default.jpg')
+    
+    
+    mehnat = models.IntegerField(default=0)
+    faoliyat = models.IntegerField(default=0)
+    project = models.IntegerField(default=0)
+    mukofot = models.IntegerField(default=0)
+    
+    
+    
+    
+    
+    def __str__(self):
+        return self.frist_name + ' ' + self.last_name
+    
+    def fullname(self):
+        return self.frist_name + ' ' + self.last_name
+    
+    class Meta:
+        verbose_name="Men haqimda ma'lumot"
+        verbose_name_plural = "Men haqimda ma'lumotlar"
+        
+class Service(models.Model):
+    images = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name="Mening xizmatlarim"
+        verbose_name_plural = "Men haqimda xizmatlarim"
+
+class Document(models.Model):
+    title = models.CharField(max_length=50)
+    file = models.FileField(upload_to='documents/')
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name="Mening hujjatlar"
+        verbose_name="Mening hujjatlarim"
+        
