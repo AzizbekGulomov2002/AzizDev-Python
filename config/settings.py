0a2cc7b5cc580ecc95af1c18a8245d14c3a38506
Azizbek <azizbekgulomov>
2022-05-03 05:46:25 +0500

    o'zgarish

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-cts$&*d!!y(8@25oay-(^^%uiv(k5yeczeg1sr4r^df=*5e+bq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
