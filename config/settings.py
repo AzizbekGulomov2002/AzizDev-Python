47a2af0d9957fccf7eb31d907d574f0fe253ddcc
Azizbek <azizbekgulomov>
2022-05-05 06:17:40 +0500

    Aziz

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,9 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    str(BASE_DIR.joinpath('static'))
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+# STATICFILES_DIRS = [
+#     str(BASE_DIR.joinpath('static'))
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
