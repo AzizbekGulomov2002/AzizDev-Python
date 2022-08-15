a3ca6ad718c535d0dd64650296ac3fadfa73f34d
Azizbek <azizbekgulomov>
2022-08-15 12:49:07 +0500

    new_deploy

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,8 @@
 
 import os
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,12 +128,11 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+django_heroku.settings(locals())
 
 
 
