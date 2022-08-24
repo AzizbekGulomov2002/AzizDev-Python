ede51afc6c038dfd074cad1652e73aab6edb1f9f
Azizbek <azizbekgulomov>
2022-08-24 23:28:31 +0500

    'media_and_static_root_editing'

--- config/settings.py
+++ config/settings.py
@@ -127,20 +127,27 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = 'media-files'
 
 
+import os
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#         BASE_DIR /'static'
+# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = 'media-files'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
