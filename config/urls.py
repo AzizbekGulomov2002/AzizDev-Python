91a6f8d4521eec964a3d562dffe98ff5233cbc8b
Azizbek <azizbekgulomov>
2022-08-25 22:25:37 +0500

    'adding_migrations'

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user.urls')),
+    path('v1/user/',include('user.urls'))
     
    
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import Index
 
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
-    
+    path('v1/user/', Index.as_view(), name='index'),
 ]
